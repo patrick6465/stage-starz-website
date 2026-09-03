3e1ca3ea1a6bbdd291edbcbfbfd266b0e453071b
patrick6465 <37958340+patrick6465@users.noreply.github.com>
2026-09-03 11:52:08 -0400

    Stage Starz mobile v1: add links.js

---
+++ mobile-app/src/links.js
@@ -0,0 +1,17 @@
+export const LINKS = {
+  website: "https://www.stagestarzdance.com",
+  parentPortal: "https://app.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgId=520608",
+  staffPortal: "https://app.jackrabbitclass.com/jr4.0/TimeClock/StaffLogin?OrgID=520608&sId=91b1edd3-a31b-4e6d-ba1d-e1be21e299f6",
+  management: "https://www.stagestarzdance.com/admin",
+  traffic: "https://www.stagestarzdance.com/admin/traffic",
+  classFinder: "https://www.stagestarzdance.com/class-finder",
+  recital: "https://www.stagestarzdance.com/recital-2026",
+  competition: "https://www.stagestarzdance.com/competition",
+  shop: "https://www.stagestarzdance.com/store",
+  parentHub: "https://www.stagestarzdance.com/parent-hub",
+  contact: "https://www.stagestarzdance.com/contact",
+  review: "https://g.page/r/Ca74tQkBw9_TEBM/review",
+  call: "tel:+17344973740",
+  email: "mailto:office@stagestarzdance.com",
+  directions: "https://www.google.com/maps/search/?api=1&query=6800+Lewis+Ave%2C+Temperance%2C+MI+48182"
+};
